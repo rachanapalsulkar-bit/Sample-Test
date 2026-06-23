841e3b918305dfd15b82f84033dd1bb5a236f5b3
Rachana Palsulkar <rachana.palsulkar@acldigital.com>
2026-06-23 15:10:18 +0530

    2nd commit

--- Test1.py
+++ Test1.py
@@ -12,20 +12,23 @@ if a>b and a>c:
     print("a is greater")
 elif b>a and b>c:
     print("b is greater")
+elif c>a and c>b:
+    print("c is greater")
+else:
+    print("Some values are equal")
 
 ##=======================##
 product = []
-product_input = input("\n Enter the product name: ")
-product.append(product_input)
-
-
-
+print("\nEnter the product name (type 'q' to stop):")
 while True:
-    product_input = input("Enter the product name: ")
+    product_input = input()
     if product_input == 'q':
         break
-    product.append(product_input)
-    print(product) ; print("Type 'q' to stop.\n")
-
+    if product_input == '0' and not product:
+        print("Cart is empty")
+        break
+    print(product)
+    
 print("All products are successfully added to the cart.")
-print("Products list is:") ; print(product)
+print("Products list is:")
+print(product)
